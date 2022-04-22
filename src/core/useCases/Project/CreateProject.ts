4da4938c0db2457f1d7b7bf3f044622d54b5fbfd
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-22 19:14:05 -0300

    edit initial attributes to create project

--- src/core/useCases/Project/CreateProject.ts
+++ src/core/useCases/Project/CreateProject.ts
@@ -1,14 +1,11 @@
 import { IProjectRepository } from '../../../infra/repositories/ProjectRepository/IProjectRepository'
 import { Project } from '../../entities/Project'
 import { ITag } from '../../entities/Tag/ITag'
-import { ITask } from '../../entities/Task/ITask'
 
 interface CreateProjectProps {
   title: string,
   description?: string,
-  image?: string,
   tags?: ITag[],
-  tasks?: ITask[]
 }
 
 export class CreateProject {
@@ -23,9 +20,7 @@ export class CreateProject {
 			status: 'to-do',
 			title: props.title,
 			description: props.description,
-			image: props.image,
 			tags: props.tags,
-			tasks: props.tasks
 		})
 
 		const project = await this.repository.createProject(newProject)
