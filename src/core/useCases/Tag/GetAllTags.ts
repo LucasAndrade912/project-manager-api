e51f7156333e2a47e124941c919781d1f03ff639
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-23 10:38:47 -0300

    add functionality to get all tags

---
+++ src/core/useCases/Tag/GetAllTags.ts
@@ -0,0 +1,15 @@
+import { ITagRepository } from '../../../infra/repositories/TagRepository/ITagRepository'
+
+export class GetAllTags {
+	private repository: ITagRepository
+
+	constructor(repository: ITagRepository) {
+		this.repository = repository
+	}
+
+	async exec() {
+		const tags = await this.repository.getAllTags()
+
+		return tags
+	}
+}
