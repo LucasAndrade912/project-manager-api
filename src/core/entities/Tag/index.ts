e85e22e4ead67be526aed47861f576c3f27dc726
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-23 10:34:15 -0300

    add validations for hexadecimal colors

--- src/core/entities/Tag/index.ts
+++ src/core/entities/Tag/index.ts
@@ -5,6 +5,13 @@ export class Tag {
 	public color: string
 
 	constructor(props: ITag) {
+		const colorPropertyContainsHashTag = props.color[0] === '#'
+		const colorPropertyIsBetween3and6CharactersLong = props.color.length >= 3 && props.color.length < 7
+
+		if (!colorPropertyContainsHashTag || !colorPropertyIsBetween3and6CharactersLong) {
+			throw new Error('Inform valid hexadecimal color')
+		}
+
 		this.tag = props.tag
 		this.color = props.color
 	}
