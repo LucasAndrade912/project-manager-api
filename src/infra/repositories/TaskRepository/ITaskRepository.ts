05847c32722f1c450ddb7f5c4cffeb8c8b15f88b
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-22 21:01:41 -0300

    create memory repository for tasks

---
+++ src/infra/repositories/TaskRepository/ITaskRepository.ts
@@ -0,0 +1,7 @@
+import { ITask } from '../../../core/entities/Task/ITask'
+
+export interface ITaskRepository {
+  createTask(task: ITask): Promise<ITask>
+  updateTask(id: number, task: { taskName?: string, finished?: boolean }): Promise<ITask>
+  getAllTasks(): Promise<ITask[]>
+}
