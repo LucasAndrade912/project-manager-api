dcc0543c667c663f9ccb83e333b86d0496d50e29
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-25 19:24:15 -0300

    create sql repository to users

---
+++ src/infra/repositories/UserRepository/SqlRepository.ts
@@ -0,0 +1,42 @@
+import { User } from '@prisma/client'
+import { IUser } from '../../../core/entities/User/IUser'
+import { prisma } from '../prisma/prismaClient'
+import { IUserRepository } from './IUserRepository'
+
+export class SqlRepository implements IUserRepository<User> {
+	async createUser(id: string): Promise<IUser> {
+		const user = await prisma.user.create({ data: { id } })
+
+		return user
+	}
+
+	async findUserById(id: string): Promise<User> {
+		const user = await prisma.user.findFirst({
+			where: { id },
+			include: {
+				projects: {
+					select: {
+						id: true,
+						title: true,
+						description: true,
+						image: true,
+						status: true,
+						created_at: true,
+						tasks: true,
+						tags: true,
+						updated_at: false,
+						user_id: false
+					}
+				}
+			}
+		})
+
+		return user
+	}
+
+	async deleteUser(id: string): Promise<void> {
+		await prisma.user.delete({
+			where: { id }
+		})
+	}
+}
