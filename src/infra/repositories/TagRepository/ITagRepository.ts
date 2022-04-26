826c5a97ca16673ce4061943b211ce45f472d61d
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-26 13:54:08 -0300

    add generic in tag repository interface

--- src/infra/repositories/TagRepository/ITagRepository.ts
+++ src/infra/repositories/TagRepository/ITagRepository.ts
@@ -1,7 +1,7 @@
 import { ITag } from '../../../core/entities/Tag/ITag'
 
-export interface ITagRepository {
-  createTag(newTag: ITag): Promise<ITag>
-  findAllTags(): Promise<ITag[]>
+export interface ITagRepository<T = ITag> {
+  createTag(tagName: string, idColor: number): Promise<ITag>
+  findAllTags(): Promise<T[]>
   deleteTag(id: number): Promise<void>
 }
