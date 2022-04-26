a7fee76e8b233dfde71301d6ab72c3ce01be71bd
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-26 14:26:02 -0300

    update tag repository interface

--- src/infra/repositories/TagRepository/ITagRepository.ts
+++ src/infra/repositories/TagRepository/ITagRepository.ts
@@ -1,7 +1,7 @@
 import { ITag } from '../../../core/entities/Tag/ITag'
 
 export interface ITagRepository<T = ITag> {
-  createTag(tagName: string, idColor: number): Promise<ITag>
+  createTag(tagName: string, idColor: number): Promise<T>
   findAllTags(): Promise<T[]>
   deleteTag(id: number): Promise<void>
 }
