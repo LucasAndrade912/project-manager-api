abcc494415c0e0eb29b0bdb2e96ef046986f1f50
Lucas Andrade <lucasantos.dev@gmail.com>
2022-04-27 20:55:43 -0300

    update project repository interface

--- src/infra/repositories/ProjectRepository/IProjectRepository.ts
+++ src/infra/repositories/ProjectRepository/IProjectRepository.ts
@@ -1,20 +1,26 @@
 import { IProject } from '../../../core/entities/Project/IProject'
-import { ITag } from '../../../core/entities/Tag/ITag'
-import { ITask } from '../../../core/entities/Task/ITask'
 
-export interface IUpdateProjectProps {
-  title?: string,
-  description?: string,
-  image?: string,
-  status?: 'to-do' | 'in-progress' | 'done'
-  tasks?: ITask[]
-  tags?: ITag[]
+export interface CreateProjectProps {
+  title?: string
+  description?: string
+  image?: string
+  status?: string | 'to-do' | 'in-progress' | 'done'
+  idTags?: number[]
 }
 
-export interface IProjectRepository {
-  createProject(project: IProject): Promise<IProject>
-  findAllProjects(): Promise<IProject[]>
-  findProjectById(id: number): Promise<IProject>
-  updateProject(id: number, changes: IUpdateProjectProps): Promise<IProject>
-  deleteProject(id: number): Promise<void>
+export interface UpdateProjectProps {
+  title?: string
+  description?: string
+  image?: string
+  status?: 'to-do' | 'in-progress' | 'done'
+  connectTags?: number[]
+  disconnectTags?: number[]
+}
+
+export interface IProjectRepository<T = IProject> {
+  createProject(newProject: CreateProjectProps, idUser: string): Promise<T>
+  findAllProjects(idUser: string): Promise<T[]>
+  findProjectById(idProject: string, idUser: string): Promise<T>
+  updateProject(idProject: string, changes: UpdateProjectProps): Promise<T>
+  deleteProject(idProject: string): Promise<void>
 }
