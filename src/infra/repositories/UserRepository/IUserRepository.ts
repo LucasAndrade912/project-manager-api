18fd1654875f2a3a0502f4e708da64c84743dcf0
Lucas Andrade <lucasantos.dev@gmail.com>
2022-05-16 19:51:32 -0300

    update user repository

--- src/infra/repositories/UserRepository/IUserRepository.ts
+++ src/infra/repositories/UserRepository/IUserRepository.ts
@@ -1,7 +1,5 @@
-import { IUser } from '../../../core/entities/User/IUser'
-
-export interface IUserRepository<T = IUser> {
-  createUser(id: string): Promise<IUser>
-  findUserById(id: string): Promise<T>
+export interface IUserRepository {
+  createUser(id: string): Promise<void>
+  findUserById(id: string): Promise<string>
   deleteUser(id: string): Promise<void>
 }
--- src/infra/repositories/UserRepository/SqlUserRepository.ts
+++ src/infra/repositories/UserRepository/SqlUserRepository.ts
@@ -1,40 +1,25 @@
-import { User } from '@prisma/client'
-import { IUser } from '../../../core/entities/User/IUser'
 import { prisma } from '../prisma/prismaClient'
 import { IUserRepository } from './IUserRepository'
 
-export class SqlUserRepository implements IUserRepository<User> {
-	async createUser(id: string): Promise<IUser> {
-		const user = await prisma.user.create({ data: { id } })
-
-		return user
+export class SqlUserRepository implements IUserRepository {
+	async createUser(id: string) {
+		await prisma.user.create({ data: { id } })
 	}
 
-	async findUserById(id: string): Promise<User> {
+	async findUserById(id: string) {
 		const user = await prisma.user.findFirst({
 			where: { id },
-			include: {
-				projects: {
-					select: {
-						id: true,
-						title: true,
-						description: true,
-						image: true,
-						status: true,
-						created_at: true,
-						tasks: true,
-						tags: true,
-						updated_at: false,
-						user_id: false
-					}
-				}
+			select: {
+				id: true,
+				created_at: false,
+				projects: false
 			}
 		})
 
-		return user
+		return user.id
 	}
 
-	async deleteUser(id: string): Promise<void> {
+	async deleteUser(id: string) {
 		await prisma.user.delete({
 			where: { id }
 		})
